f9a75c3511a5c0e0414b8441905a206c8d9a0a50
davidpwn360 <dhuang9@stuy.edu>
2017-01-10 05:55:17 -0500

    edited risk map

--- print.java
+++ print.java
@@ -6,6 +6,20 @@ public class print {
 
     public static void main(String args[]) throws FileNotFoundException {
   
+		//creating File instance to reference text file in Java
+        File bob = new File("cover.txt");
+      
+        //Creating Scanner instnace to read File in Java
+        Scanner scaner = new Scanner(bob);
+      
+        //Reading each line of file using Scanner class
+        int lineNumber1 = 1;
+        while(scaner.hasNextLine()){
+            String line1 = scaner.nextLine();
+            System.out.println(line1);
+            lineNumber1++;
+        } 
+		
         //creating File instance to reference text file in Java
         File text = new File("printfile.txt");
       
