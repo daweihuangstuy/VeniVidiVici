225e8e74ecaa358fbbb75a73f5dc78c44cd57e23
davidpwn360 <dhuang9@stuy.edu>
2017-01-10 21:32:13 -0500

    added color to maps

--- Woo.java
+++ Woo.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 import cs1.Keyboard;
 
 public class Woo {
+	
+	//project colors
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
