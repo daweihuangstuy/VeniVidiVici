2f5f40720d408c645af5e689d68e7f73bd85bc47
Yu Qi Wu <Yu Qi Wu>
2017-01-13 03:23:56 -0500

    changed the system of Territory

--- Territory/Territory.java
+++ Territory/Territory.java
@@ -1,18 +1,49 @@
 public class Territory{
-    protected int curTroops; // current amount of troops on it
-    protected String occupier; // current player occupying this territory
-
-    public Territory(){
-	curTroops = 0;
-	occupier = "null";
+    public String[][] territory = new String[42][3];
+    public Territory(){	
+	territory[0][0] = "A04";
+	territory[1][0] = "A11";
+	territory[2][0] = "A12";
+	territory[3][0] = "A13";
+	territory[4][0] = "A14";
+	territory[5][0] = "A15";
+	territory[6][0] = "A16";
+	territory[7][0] = "A17";
+	territory[8][0] = "A18";
+	territory[9][0] = "A19";
+	territory[10][0] = "B11";
+	territory[11][0] = "B12";
+	territory[12][0] = "B13";
+	territory[13][0] = "C11";
+	territory[14][0] = "C12";
+	territory[15][0] = "C13";
+	territory[16][0] = "C14";
+	territory[17][0] = "C15";
+	territory[18][0] = "C16";
+	territory[19][0] = "C17";
+	territory[20][0] = "D11";
+	territory[21][0] = "D12";
+	territory[22][0] = "D13";
+	territory[23][0] = "D14";
+	territory[24][0] = "D15";
+	territory[25][0] = "D16";
+	territory[26][0] = "E11";
+	territory[27][0] = "E12";
+	territory[28][0] = "E13";
+	territory[29][0] = "E14";
+	territory[30][0] = "E15";
+	territory[31][0] = "E16";
+	territory[32][0] = "E17";
+	territory[33][0] = "E18";
+	territory[34][0] = "E19";
+	territory[35][0] = "E21";
+	territory[36][0] = "E22";
+	territory[37][0] = "E23";
+	territory[38][0] = "F11";
+	territory[39][0] = "F12";
+	territory[40][0] = "F13";
+	territory[41][0] = "F14";
     }
-
-    public int getCurTroops(){
-	return curTroops;
-    }
-
-    public String getOccupier(){
-	return occupier;
-    }
+    
 }
     
