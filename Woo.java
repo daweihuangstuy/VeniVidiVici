3ddc72d8ea1fafba73702d275afb3f0d39e8fb4f
fabiola <fradosav@stuy.edu>
2017-01-16 15:38:52 -0500

    move feature

--- Woo.java
+++ Woo.java
@@ -333,6 +333,7 @@ public class Woo {
 	    numPlay -= 1;
 	}
 	
+    
     }
     
     //*******************************************************************************
@@ -511,6 +512,29 @@ public class Woo {
 	    //print map line (final procedure)
 	    System.out.println(line2);
 	}
+
+	//for move feature
+
+		//move feature
+    if (playerTurn.equals("player1")){
+	occupied = player1Occupied;
+    }
+    if (playerTurn.equals("player2")){
+	occupied = player2Occupied;
+    }
+    if (playerTurn.equals("player3")){
+	occupied = player3Occupied;
+    }
+    if (playerTurn.equals("player4")){
+	occupied = player4Occupied;
+    }
+    if (playerTurn.equals("player5")){
+	occupied = player5Occupied;
+    }
+    if (playerTurn.equals("player6")){
+	occupied = player6Occupied;
+    }
+
     }
     //*******************************************************************************
     //helpful methods
@@ -575,6 +599,30 @@ public class Woo {
 	    }
 	}	    
     }
+
+
+    public static void move(ArrayList<String> occupied, int numTroops, String origin, String destination){
+	for ( String terr : occupied ){
+	    if (origin != terr){
+		System.out.println("Sorry, you do not occupy " + origin + ". Please choose another territory to move your troops from.");
+	    }
+	}
+	
+	for ( String terr : occipied ) {
+	    if (destination != terr) {
+		System.out.println("Sorry, you do not occupy " + destination + ". Please choose another territory to move your troops to.");
+	    }
+	}
+	//	if (numTroops @ origin  > 0){
+	    //troops @ origin -= numToops;
+	    //troops @destination += numTroops;
+
+	}
+	else{
+	    System.out.println("Sorry, you do not have enough troops to move to the destination. Please try moving troops from a different territory.");
+
+    }
+    
     public static void update(String player){
 	//player troopNumbers
 	player1TroopNum = Territory.territoryNumber(player);
